5f11bdbb05d3cb430e3b313f758c2892e1a02251
Patricia B. Sutto <paty_100bs@hotmail.com>
2025-05-14 01:34:40 -0300

    Função get_production_data criada e app.py adaptado

--- app.py
+++ app.py
@@ -1,17 +1,45 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, redirect, render_template_string, url_for
 from flasgger import Swagger
-import pandas as pd
-import json
+from utils.GetEmbrapaData import GetEmbrapaData
 
-# Criação do app Flask e configuração do Swagger
 app = Flask(__name__)
-
-app.config['SWAGGER'] = {
-    'title': '5MLET - Fase 1',
-    'uiversion': 3
-}
-
 swagger = Swagger(app)
+
+# Página de login com formulário HTML básico
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Aqui poderia validar usuário/senha, mas vamos direto redirecionar
+        return redirect(url_for('flasgger.apidocs'))
+    
+    # HTML do formulário
+    return render_template_string("""
+    <!doctype html>
+    <html>
+    <head><title>Login</title></head>
+    <body>
+        <h2>Login</h2>
+        <form method="post">
+            <label>Usuário:</label><br>
+            <input type="text" name="username"><br>
+            <label>Senha:</label><br>
+            <input type="password" name="password"><br><br>
+            <input type="submit" value="Entrar">
+        </form>
+    </body>
+    </html>
+    """)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    """
+    Rota de saudação
+    ---
+    responses:
+      200:
+        description: Retorna uma saudação
+    """
+    return jsonify(message="Olá, mundo!")
 
 @app.route('/soma', methods=['GET'])
 def soma():
@@ -23,131 +51,56 @@ def soma():
         in: query
         type: number
         required: true
-        description: Primeiro número a ser somado
       - name: b
         in: query
         type: number
         required: true
-        description: Segundo número a ser somado
     responses:
       200:
-        description: Resultado da soma
-        schema:
-          type: object
-          properties:
-            resultado:
-              type: number
-              example: 8.0
+        description: O resultado da soma
     """
-    a = float(request.args.get('a'))
-    b = float(request.args.get('b'))
-    resultado = a + b
-    return jsonify({'resultado': resultado})
+    a = float(request.args.get("a", 0))
+    b = float(request.args.get("b", 0))
+    return jsonify(resultado=a + b)
 
-@app.route('/multiplicacao', methods=['GET'])
-def multiplicacao():
+
+@app.route('/get_production_data', methods=['GET'])
+def get_production_data():
     """
-    Multiplica dois números
+    Obtém dados de produção de uva da Embrapa.
     ---
     parameters:
-      - name: x
+      - name: ano
         in: query
         type: number
         required: true
-        description: Primeiro número a ser multiplicado
-      - name: y
-        in: query
-        type: number
-        required: true
-        description: Segundo número a ser multiplicado
+    
     responses:
       200:
-        description: Resultado da multiplicação
+        description: Dados de produção de uva
         schema:
           type: object
           properties:
-            resultado:
+            categoria:
+              type: object
+              description: Dicionário com os dados da categoria e seus subitens
+            total:
               type: number
-              example: 15.0
-    """
-    x = float(request.args.get('x'))
-    y = float(request.args.get('y'))
-    resultado = x * y
-    return jsonify({'resultado': resultado})
+              description: Total da categoria
+      500:
+        description: Internal Server Error. Erro inesperado 
 
-@app.route('/web', methods=['GET'])
-def web():
+      502:
+        description: 502 Bad Gateway. Falhas na comunicação com serviços.
     """
-    Teste simples da API
-    ---
-    responses:
-      200:
-        description: Retorna uma resposta simples
-        schema:
-          type: object
-          properties:
-            resultado:
-              type: string
-              example: "ok"
-    """
-    return jsonify({'resultado': 'ok'})
+    ano = float(request.args.get("ano", 0))
 
-@app.route('/web2', methods=['GET'])
-def web2():
-    """
-    Teste simples da API
-    ---
-    parameters:
-      - name: Domínio
-        in: query
-        type: string
-        required: true
-        default: "http://vitibrasil.cnpuv.embrapa.br/"
-        description: Domínio do site. Exemplo - http://vitibrasil.cnpuv.embrapa.br/
-      - name: Caminho
-        in: query
-        type: string
-        required: true
-        description: Caminho para uma parte específica de um site. Exemplo - index.php?opcao=opt_03
-
-    responses:
-      200:
-        description: Retorna um dataframe.
-        schema:
-          type: object
-          properties:
-            resultado:
-              type: string
-              example: "ok"
-    """
-
-    # dominio = str(request.args.get('Dominio', default='http://vitibrasil.cnpuv.embrapa.br/'))
-    # caminho = str(request.args.get('Caminho'))
-   
-    # return jsonify({'resultado': 'ok'})
-
-     # Criando um DataFrame de exemplo com pandas
-    data = {
-        'coluna1': ['valor1', 'valor2', 'valor3'],
-        'coluna2': ['valorA', 'valorB', 'valorC']
-    }
-    df = pd.DataFrame(data)
+    # Aqui você chamaria a função que obtém os dados da Embrapa
+    # Exemplo de chamada:
+    dados = GetEmbrapaData().get_production_data(ano)
     
-    # Convertendo o DataFrame para JSON
-    df_json = df.to_json(orient='records')  # 'records' cria uma lista de dicionários
-
-    # Carregar a string JSON em uma lista de objetos
-    df_json = json.loads(df_json)
     
-    # Retornando o DataFrame como resposta JSON
-    return jsonify({'resultado': 'ok', 'data': df_json})
-
-
-
-
-
-
-
+    return jsonify(dados)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils/GetEmbrapaData.py
+++ utils/GetEmbrapaData.py
@@ -1,5 +1,89 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+from io import StringIO
+import json
+
 
 class GetEmbrapaData():
 
-    def get_data():
+    def __init__(self):
+        self.dominio = 'http://vitibrasil.cnpuv.embrapa.br/'
+    
+
+    def get_production_data(self, ano):
+        try:
+            complemento = f"index.php?ano={ano}&opcao=opt_02"
+
+            # Faz a requisição para a página
+            response = requests.get(self.dominio+complemento)
+
+            # Garante a correta leitura de caracteres acentuados
+            response.encoding = 'utf-8'
+
+            # Verifica se a página foi acessada com sucesso
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                
+                tabela = soup.find('table', class_='tb_base tb_dados')
+
+                dados_json = {}
+                categoria_atual = None
+
+                for row in tabela.find('tbody').find_all('tr'):
+                    cols = [col.text.strip() for col in row.find_all('td')]
+                    if len(cols) != 2:
+                        continue
+                    nome, quantidade = cols
+
+                    # Verifica se é categoria principal
+                    if 'tb_item' in row.find('td')['class']:
+                        categoria_atual = nome
+                        dados_json[categoria_atual] = {'total': quantidade}
+
+                    # Verifica se é subitem
+                    elif 'tb_subitem' in row.find('td')['class']:
+                        if categoria_atual:
+                            dados_json[categoria_atual][nome] = quantidade
+
+                # Adiciona o total geral da tabela (rodapé)
+                tfoot = tabela.find('tfoot')
+                if tfoot:
+                    total_row = [td.text.strip() for td in tfoot.find_all('td')]
+                    if len(total_row) == 2:
+                        dados_json['TOTAL GERAL'] = {total_row[0]: total_row[1]}
+
+                # Salva o JSON estruturado
+                # json_file = f"dados_producao_{ano}.json"
+                # with open(json_file, 'w', encoding='utf-8') as f:
+                #     json.dump(dados_json, f, ensure_ascii=False, indent=2)
+
+                # print(f"JSON estruturado salvo como: {json_file}")
+
+            else:
+                print(f"Erro ao acessar a página. Código HTTP: {response.status_code}")
+
+            if len(dados_json) > 0:
+                dados_json['ano'] = ano
+
+            return dados_json
+        
+        except requests.exceptions.RequestException as e:
+            return {"erro": f"Erro na requisição HTTP: {str(e)}"}
+        
+        except Exception as e:
+            return {"erro": f"Erro inesperado: {str(e)}"}
+
+    
+      
+    def get_exportation_data():
+        pass
+
+    def get_commercialization_data():
+        pass
+
+    def get_importation_data():
+        pass
+
+    def get_processing_data():
         pass
