f9d732693691cb4ba29044ef31788f994eb09de0
Patricia B. Sutto <paty_100bs@hotmail.com>
2025-05-19 01:33:51 -0300

    rotas finalizadas

--- app.py
+++ app.py
@@ -30,40 +30,6 @@ def login():
     </html>
     """)
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    """
-    Rota de saudação
-    ---
-    responses:
-      200:
-        description: Retorna uma saudação
-    """
-    return jsonify(message="Olá, mundo!")
-
-@app.route('/soma', methods=['GET'])
-def soma():
-    """
-    Soma dois números
-    ---
-    parameters:
-      - name: a
-        in: query
-        type: number
-        required: true
-      - name: b
-        in: query
-        type: number
-        required: true
-    responses:
-      200:
-        description: O resultado da soma
-    """
-    a = float(request.args.get("a", 0))
-    b = float(request.args.get("b", 0))
-    return jsonify(resultado=a + b)
-
-
 @app.route('/get_production_data', methods=['GET'])
 def get_production_data():
     """
@@ -87,6 +53,9 @@ def get_production_data():
             total:
               type: number
               description: Total da categoria
+            ano:
+              type: number
+              description: Ano da coleta dos dados
       500:
         description: Internal Server Error. Erro inesperado 
 
@@ -95,12 +64,214 @@ def get_production_data():
     """
     ano = float(request.args.get("ano", 0))
 
-    # Aqui você chamaria a função que obtém os dados da Embrapa
-    # Exemplo de chamada:
+    # Chamada da função que obtém os dados da Embrapa
     dados = GetEmbrapaData().get_production_data(ano)
-    
-    
+
+    if 'erro' in dados:
+        if dados['erro'] == "Erro na requisição HTTP: 502 Bad Gateway":
+            return jsonify({"erro": "502 Bad Gateway. Falhas na comunicação com serviços."}), 502
+        else:
+            return jsonify({"erro": "Erro inesperado"}), 500
+      
     return jsonify(dados)
+
+@app.route('/get_processing_data', methods=['GET'])
+def get_processing_data():
+    """
+    Obtém dados de processamento de uva da Embrapa.
+    ---
+    parameters:
+      - name: ano
+        in: query
+        type: number
+        required: true
+      - name: opcao
+        in: query
+        type: string
+        required: true
+        enum: ['Viníferas', 'Americanas e híbridas', 'Uvas de mesa', 'Sem classificação']
+    
+    responses:
+        200:
+          description: Dados de processamento de uva
+          schema:
+            type: object
+            properties:
+              categoria:
+                type: object
+                description: Dicionário com os dados da categoria e seus subitens
+              total:
+                type: number
+                description: Total da categoria
+              ano:
+                type: number
+                description: Ano da coleta dos dados
+        500:
+          description: Internal Server Error. Erro inesperado 
+
+        502:
+          description: 502 Bad Gateway. Falhas na comunicação com serviços.
+    """
+    ano = float(request.args.get("ano", 0))
+    opcao = request.args.get("opcao", "")
+
+    print(opcao)
+
+    # Chamada da função que obtém os dados da Embrapa
+    dados = GetEmbrapaData().get_processing_data(ano, opcao)
+
+    if 'erro' in dados:
+        if dados['erro'] == "Erro na requisição HTTP: 502 Bad Gateway":
+            return jsonify({"erro": "502 Bad Gateway. Falhas na comunicação com serviços."}), 502
+        else:
+            return jsonify({"erro": "Erro inesperado"}), 500
+      
+    return jsonify(dados)
+   
+@app.route('/get_commercialization_data', methods=['GET'])
+def get_commercialization_data():
+    """
+    Obtém dados de comercialização de uva da Embrapa.
+    ---
+    parameters:
+      - name: ano
+        in: query
+        type: number
+        required: true
+    
+    responses:
+      200:
+        description: Dados de comercialização de uva
+        schema:
+          type: object
+          properties:
+            categoria:
+              type: object
+              description: Dicionário com os dados da categoria e seus subitens
+            total:
+              type: number
+              description: Total da categoria
+            ano:
+              type: number
+              description: Ano da coleta dos dados
+      500:
+        description: Internal Server Error. Erro inesperado 
+
+      502:
+        description: 502 Bad Gateway. Falhas na comunicação com serviços.
+    """
+    ano = float(request.args.get("ano", 0))
+
+    # Chamada da função que obtém os dados da Embrapa
+    dados = GetEmbrapaData().get_commercialization_data(ano)
+
+    if 'erro' in dados:
+        if dados['erro'] == "Erro na requisição HTTP: 502 Bad Gateway":
+            return jsonify({"erro": "502 Bad Gateway. Falhas na comunicação com serviços."}), 502
+        else:
+            return jsonify({"erro": "Erro inesperado"}), 500
+      
+    return jsonify(dados)
+
+@app.route('/get_importation_data', methods=['GET'])
+def get_importation_data():
+    """
+    Obtém dados de importação de uva da Embrapa.
+    ---
+    parameters:
+      - name: ano
+        in: query
+        type: number
+        required: true
+      - name: opcao
+        in: query
+        type: string
+        required: true
+        enum: ['Vinhos de mesa', 'Espumantes', 'Uvas frescas', 'Uvas passas', 'Suco de uva']
+    
+    responses:
+        200:
+          description: Dados de importação de uva
+          schema:
+            type: array
+            items:
+              type: object
+            example:
+              - pais: "Argentina"
+                quantidade: 123
+                ano: 2023
+             
+        500:
+          description: Internal Server Error. Erro inesperado 
+
+        502:
+          description: 502 Bad Gateway. Falhas na comunicação com serviços.
+    """
+    ano = float(request.args.get("ano", 0))
+    opcao = request.args.get("opcao", "")
+
+    # Chamada da função que obtém os dados da Embrapa
+    dados = GetEmbrapaData().get_importation_data(ano, opcao)
+
+    if 'erro' in dados:
+        if dados['erro'] == "Erro na requisição HTTP: 502 Bad Gateway":
+            return jsonify({"erro": "502 Bad Gateway. Falhas na comunicação com serviços."}), 502
+        else:
+            return jsonify({"erro": f"Erro inesperado {dados}"}), 500
+      
+    return jsonify(dados)
+    # return jsonify({"message": f"Dados de processamento de uva {opcao} para o ano {ano}."})
+
+
+@app.route('/get_exportation_data', methods=['GET'])
+def get_exportation_data():
+    """
+    Obtém dados de exportação de uva da Embrapa.
+    ---
+    parameters:
+      - name: ano
+        in: query
+        type: number
+        required: true
+      - name: opcao
+        in: query
+        type: string
+        required: true
+        enum: ['Vinhos de mesa', 'Espumantes', 'Uvas frescas', 'Suco de uva']
+    
+    responses:
+        200:
+          description: Dados de exportação de uva
+          schema:
+            type: array
+            items:
+              type: object
+            example:
+              - pais: "Argentina"
+                quantidade: 123
+                ano: 2023
+             
+        500:
+          description: Internal Server Error. Erro inesperado 
+
+        502:
+          description: 502 Bad Gateway. Falhas na comunicação com serviços.
+    """
+    ano = float(request.args.get("ano", 0))
+    opcao = request.args.get("opcao", "")
+
+    # Chamada da função que obtém os dados da Embrapa
+    dados = GetEmbrapaData().get_exportation_data(ano, opcao)
+
+    if 'erro' in dados:
+        if dados['erro'] == "Erro na requisição HTTP: 502 Bad Gateway":
+            return jsonify({"erro": "502 Bad Gateway. Falhas na comunicação com serviços."}), 502
+        else:
+            return jsonify({"erro": f"Erro inesperado {dados}"}), 500
+      
+    return jsonify(dados)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
